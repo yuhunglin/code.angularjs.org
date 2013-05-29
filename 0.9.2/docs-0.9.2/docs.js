30972e5e37d0b84a8608439ec62af948469a2ad2
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-05-29 05:21:10 -0700

    Purging old unused files

--- 0.9.2/docs-0.9.2/docs.js
+++
@@ -1,7 +0,0 @@
-function DocController($resource, $location){
-  this.docs = $resource('documentation.json').get();
-  this.getPartialDoc = function(){
-    return encodeURIComponent($location.hashPath) + '.html';
-  };
-}
-DocController.$inject=['$resource', '$location'];
